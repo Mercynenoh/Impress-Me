61ce6493da10fbb041772a0155ca363fe425eff2
Mercynenoh <mercychepngenoh74@gmail.com>
2022-05-15 00:08:52 +0300

    Deployment

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
 class ProdConfig(Config):
     '''Child
     '''
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
 DEBUG = True
 
 
